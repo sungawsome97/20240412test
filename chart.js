87b804dff99ad3914a1b0872370a1073b2e9019a
sungawsome97 <tjddk1991@naver.com>
2024-04-14 21:21:27 +0900

    init

--- chart.js
+++ chart.js
@@ -1,1 +1,13 @@
-let a = 
+
+//상품정보
+let products = [
+    {id: 0, price: 70000, title: "blossom dress" }
+    {id: 1, price: 50000, title: "stripe shirt"}
+    {id: 2, price: 30000, title: "short pants"}];
+  
+let chart.html = ""
+for (let i = 0; i < products.length; i++){
+    let products= products[i]
+    Chart.html;
+
+}
