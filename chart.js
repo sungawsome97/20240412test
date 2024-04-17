46a160dc90b1373ae9e1cb20756eba0933e9ab32
sungawsome97 <tjddk1991@naver.com>
2024-04-18 00:46:15 +0900

    init

--- chart.js
+++ chart.js
@@ -11,3 +11,14 @@ for (let i = 0; i < products.length; i++){
     Chart.html;
 
 }
+
+//문제 1) concat 파일 쓰기 concat(let products)
+
+//문제 2) const filterUserList = userList.filter((user) => prics > 50000) console.log(filteruserlist)
+
+//문제 3) const products map = {id:3, price:45000, title:"flower pants"}
+
+
+//문제 4) {some products >= 30000}
+
+//문제 5) arr push(id:4, price:63000, title: "animal patten jacket")
